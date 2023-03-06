701ed6a99ee6e8732ac10de07bef732af11c0c36
Octracus <julianannor1@gmail.com>
2023-03-05 19:22:24 -0800

    More C

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void) //This function displays lowercase and then uppercase alphabets//
+int main(void) /*This function displays lowercase and then uppercase alphabets*/
 {
 	char c;
 
