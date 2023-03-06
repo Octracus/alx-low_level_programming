77636c3e9bef47f7d1ede4ae6f7018b97bf88c68
Octracus <julianannor1@gmail.com>
2023-03-05 19:13:51 -0800

    More C

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-       	char c;
+	char c;
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
-       	for (c = 'A'; c <= 'Z'; c++)
-	       	putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
 	putchar('\n');
 	return (0);
 }
