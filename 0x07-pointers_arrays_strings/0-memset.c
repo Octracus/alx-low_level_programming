4e6678fc3ebe0a8e1a9a7f2d1906ab37b2a3d3fc
Octracus <julianannor1@gmail.com>
2023-03-07 09:41:41 -0800

    More C

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,17 +3,17 @@
 
 /**
  * _memset - fills memory with a constant byte
- * s: pointer to the memory area to fill
- * b: constant byte to fill with
- * n: number of bytes to fill
+ * @s: pointer to the memory area to fill
+ * @b: constant byte to fill memory with
+ * @n: number of bytes to fill
  *
- * Return: pointer to the filled memory area
+ * Return: pointer to the filled memory area @s
  */
-int main()
+char *_memset(char *s, char b, unsigned int n)
 {
-	char buffer[BUFSIZ];
+	unsigned int i;
 
-	memset(buffer, '\0', BUFSIZ);
-	puts("Buffer initialized");
-	return(0);
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
 }
