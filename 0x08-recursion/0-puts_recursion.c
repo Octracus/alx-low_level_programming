512da8adc566abca2d8f7c2b57457494bc20972a
Octracus <julianannor1@gmail.com>
2023-08-30 21:32:12 -0700

    C C C C na na ko

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include main.h
+/**
+ * Function to print a string followed by a new line
+ * Return: Always 0 (Success
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
