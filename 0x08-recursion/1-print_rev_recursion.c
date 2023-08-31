3f082c7e30c6ccdbde36c8eebe82313586cff03e
Octracus <julianannor1@gmail.com>
2023-08-30 22:58:10 -0700

    C C C C na na ko

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *Function that prints a string in reverse
+ *_print_rev_recursion - prints a string in reverse
  * s: String to be printed.
  */
 void _print_rev_recursion(char *s)
