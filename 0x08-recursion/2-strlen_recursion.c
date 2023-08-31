7332b1437f6a6caead92bc23bdd8d8e8fea6dc67
Octracus <julianannor1@gmail.com>
2023-08-30 23:11:32 -0700

    C C C C na na ko

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,6 +2,7 @@
 /**
  * _strlen_recursion -  returns the length of a string
  * @s: String to be measured
+ * Return the length of a string
  */
 int _strlen_recursion(char *s)
 {
@@ -11,5 +12,5 @@ int _strlen_recursion(char *s)
 		longit++;
 		longit += _strlen_recursion(s + 1);
 	}
-	retrun (longit);
+	return (longit);
 }
